54807b3ed34017d1d94227bcb0f70982c4585041
Alexis Métaireau <alexis@notmyidea.org>
2014-07-16 13:21:05 +0200

    Add some tests for HawkAuth

--- requests_hawk/tests/test_hawkauth.py
+++ requests_hawk/tests/test_hawkauth.py
@@ -5,5 +5,21 @@ import unittest
 class TestHawkAuth(unittest.TestCase):
 
     def test_hawkauth_errors_when_credentials_and_hawk_session_passed(self):
-        self.assertRaise(AttributeError, HawkAuth,
-                         credentials={}, hawk_session="test")
+        self.assertRaises(AttributeError, HawkAuth,
+                          credentials={}, hawk_session="test")
+
+    def test_hawkauth_errors_when_no_auth_is_set(self):
+        self.assertRaises(AttributeError, HawkAuth)
+
+    def test_key_non_hex_values_throws(self):
+        self.assertRaises(TypeError, HawkAuth, hawk_session="test")
+
+    def test_credentials_are_derived_from_session(self):
+        auth = HawkAuth(hawk_session="hello".encode("hex"))
+        self.assertDictEqual(auth.credentials, {
+            'id': '15064c77e946608226a9c2d8da61ac5e0e85f325334965c68a3f47e8091'
+                  'f8412',
+            'key': 'cb3829c6d6fe3f58609d58f09818295dfbdf45803ec50b8d66c4132f7a'
+                   'd14aa0',
+            'algorithm': 'sha256'
+        })
