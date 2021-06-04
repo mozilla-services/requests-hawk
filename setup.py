39ce218d3c4a330f64f7046f4b37a2312b069ff6
Rémy HUBSCHER <hubscher.remy@gmail.com>
2021-06-04 19:00:25 +0200

    Back to development: 1.2.0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with codecs.open(os.path.join(here, 'CHANGES.txt'), encoding='utf-8') as f:
 requires = ['requests', 'mohawk']
 
 setup(name='requests-hawk',
-      version='1.1.1',
+      version='1.2.0.dev0',
       description='requests-hawk',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
